fd1ce8bc627cb26fec679133a6536598be791906
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2023-04-11 23:34:15 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -31,5 +31,5 @@ const AppLayout = () => {
   )
 };
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")); 
 root.render(<AppLayout />);
