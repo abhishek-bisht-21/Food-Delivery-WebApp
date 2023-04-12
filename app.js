227ff881099bf5d56af5923d6a62f228a67f59f1
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2023-04-13 01:57:03 +0530

    Added Res Image and Styling

--- app.js
+++ app.js
@@ -21,30 +21,31 @@ import ReactDOM from "react-dom/client";
  */
 
 const Header = () => {
-  return (
-    <div className="header">
-      <div className="logo-container">
-        <img
-          className="logo"
-          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
-        />
-      </div>
+	return (
+		<div className="header">
+			<div className="logo-container">
+				<img
+					className="logo"
+					src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+				/>
+			</div>
 
-      <div className="nav-items">
-        <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
-          <li>Cart</li>
-        </ul>
-      </div>
-    </div>
-  );
+			<div className="nav-items">
+				<ul>
+					<li>Home</li>
+					<li>About Us</li>
+					<li>Contact Us</li>
+					<li>Cart</li>
+				</ul>
+			</div>
+		</div>
+	);
 };
 
 const RestraurantCard = () => {
 	return (
-		<div className="res-card">
+		<div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+			<img className="res-logo" alt="res-logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdCggSUwCg0vfyJDtYM_FpfKJjP06rpL21xhSuHUEAsQ&s" />
 			<h3>Faasos</h3>
 		</div>
 	);
@@ -52,7 +53,7 @@ const RestraurantCard = () => {
 
 const Body = () => {
 	return (
-		<div className = "body">
+		<div className="body">
 			<div className="search">Search</div>
 			<div className="res-container">
 				<RestraurantCard />
@@ -62,13 +63,13 @@ const Body = () => {
 };
 
 const AppLayout = () => {
-  return (
-  <div className="app">
-    <Header />
-    <Body />
-  </div>
-  )
+	return (
+		<div className="app">
+			<Header />
+			<Body />
+		</div>
+	)
 };
 
-const root = ReactDOM.createRoot(document.getElementById("root")); 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<AppLayout />);
